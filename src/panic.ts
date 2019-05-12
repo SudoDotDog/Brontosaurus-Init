1eddab5641aeba01f2a3bde90c9ef7a682f1cec4
WMXPY <wm@mengw.io>
2019-05-11 20:57:18 -0500

    fix ci

--- src/panic.ts
+++ src/panic.ts
@@ -10,10 +10,12 @@ export const MODULE_NAME = 'Brontosaurus-Init';
 
 export enum ERROR_CODE {
 
+    DATABASE_LINK_NOT_ASSIGNED = 1005,
 }
 
 export const ERROR_LIST: Record<ERROR_CODE, string> = {
 
+    [ERROR_CODE.DATABASE_LINK_NOT_ASSIGNED]: 'Database link environment not assigned',
 };
 
 export const panic: Panic<ERROR_CODE> = Panic.withDictionary(MODULE_NAME, ERROR_LIST);
